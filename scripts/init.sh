42a18c4e4e3248701b41a174256bd7c337b0f2fd
Andrei Navoichyk <andrei@cere.io>
2021-03-25 13:07:53 +0300

    Revert "clean up"
    This reverts commit a14584be

---
+++ scripts/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "*** Initializing WASM build environment"
+
+if [ -z $CI_PROJECT_NAME ] ; then
+   rustup update stable
+fi
+
+rustup target add wasm32-unknown-unknown --toolchain stable

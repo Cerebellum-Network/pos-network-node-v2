c4b99f31597bc4fa2486c8db0195f6229b3ddebd
Andrei Navoichyk <andrei@cere.io>
2021-03-25 16:33:07 +0300

    clean up

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,8 +4,4 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-fi
-
 rustup target add wasm32-unknown-unknown --toolchain nightly
